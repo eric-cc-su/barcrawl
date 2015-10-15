cdf28344e44a63c2fa6e2d95cbef8bd928ff0498
Eric <eric.cc.su@gmail.com>
2015-10-15 14:28:22 -0400

    Debug false, AWS configurations

--- barcrawl/barcrawl/settings.py
+++ barcrawl/barcrawl/settings.py
@@ -24,9 +24,9 @@ secretFile = open(secret)
 SECRET_KEY = secretFile.readline().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.elasticbeanstalk.com']
 
 # Application definition
 
